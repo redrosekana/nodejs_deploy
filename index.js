2011bef0c62cc696dc6cf150e7bb230dc73f0225
kana <lnbtkana2326@gmail.com>
2022-08-01 09:03:36 +0700

    test5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("")
 const port = process.env.PORT || 8000
+
 
 app.get('/',(request,response)=>{
     response.send("test api")
@@ -10,11 +12,19 @@ app.get('/api/test',(request,response)=>{
     response.json([
         {
             title:"lorem 1",
-            content:"kana is bad"
+            content:"kana is good"
         },
         {
             title:"lorem 2",
-            content:"kana is good"
+            content:"jack is good"
+        },
+        {
+            title:"lorem 3",
+            content:"net is bad"
+        },
+        {
+            title:"lorem 4",
+            content:"non is good"
         }
     ])
 })
