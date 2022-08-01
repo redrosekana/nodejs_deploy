37b226a66aa6389c1bcb49605636fe980b7fe398
kana <lnbtkana2326@gmail.com>
2022-08-01 22:20:58 +0700

    test

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser")
 const Customer = require('./customer.js')
 const cors = require("cors")
 const app = express()
-
 const port = process.env.PORT || 8000
+
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
